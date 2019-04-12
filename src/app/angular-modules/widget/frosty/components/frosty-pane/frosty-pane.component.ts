f3672a07e5b861003d371fc3994b943d671436ea
James Pickup <jamespickup2@gmail.com>
2019-04-12 14:09:40 +0100

    Frosty pane to pass in image rather than be hardcoded.

--- src/app/angular-modules/widget/frosty/components/frosty-pane/frosty-pane.component.ts
+++ src/app/angular-modules/widget/frosty/components/frosty-pane/frosty-pane.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-frosty-pane',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./frosty-pane.component.scss']
 })
 export class FrostyPaneComponent implements OnInit {
+
+  @Input()
+  public imageUrl = '';
 
   constructor() { }
 
