a002c41d472d82aeb08b8fa8880631c341c3b460
James Pickup <jamespickup2@gmail.com>
2019-04-13 22:15:42 +0100

    No user select on the lock screen

--- src/app/angular-modules/service/fullscreen/services/fullscreen.service.ts
+++ src/app/angular-modules/service/fullscreen/services/fullscreen.service.ts
@@ -8,21 +8,33 @@ export class FullscreenService {
   constructor() { }
 
   public enterFullscreen(element: ElementRef<HTMLElement>) {
+    // tslint:disable-next-line: no-string-literal
     if (element['requestFullscreen']) {
+      // tslint:disable-next-line: no-string-literal
       element['requestFullscreen']();
+      // tslint:disable-next-line: no-string-literal
     } else if (element['mozRequestFullScreen']) {
+      // tslint:disable-next-line: no-string-literal
       element['mozRequestFullScreen']();
+      // tslint:disable-next-line: no-string-literal
     } else if (element['webkitRequestFullscreen']) {
+      // tslint:disable-next-line: no-string-literal
       element['webkitRequestFullscreen'](element['ALLOW_KEYBOARD_INPUT']);
+      // tslint:disable-next-line: no-string-literal
     } else if (element['msRequestFullscreen']) {
+      // tslint:disable-next-line: no-string-literal
       element['msRequestFullscreen']();
     }
   }
 
   public deviceSupportsFullscreenRequest(element: ElementRef<HTMLElement>): boolean {
+    // tslint:disable-next-line: no-string-literal
     return (element.nativeElement['requestFullscreen']
+      // tslint:disable-next-line: no-string-literal
       || element.nativeElement['mozRequestFullScreen']
+      // tslint:disable-next-line: no-string-literal
       || element.nativeElement['webkitRequestFullscreen']
+      // tslint:disable-next-line: no-string-literal
       || element.nativeElement['msRequestFullscreen']);
   }
 }
